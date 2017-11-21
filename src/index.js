198cced86aa2e48d1a181ac801a07ffc25d37cb3
Giorgio Borelli <giorgio.borelli@abstract.it>
2017-11-21 09:31:59 +0100

    add complexity to default app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
 import './main.scss';
 
 
-const App = prop => (
-  <div className={prop.className}>Test #1</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      counter: 0,
+    };
+  }
 
+  handleClick(e) {
+    e.preventDefault();
+    const counter = this.state.counter + 1;
+    this.setState({ counter });
+  }
+
+  render() {
+    return (
+      <div className={this.props.className}>
+        <span>{this.state.counter}</span>{' '}
+        <button onClick={this.handleClick}>Add 1</button>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  className: PropTypes.string,
+};
+
+App.defaultProps = {
+  className: 'main-wrapper',
+};
 
 ReactDOM.render(
-  <App className="main-wrapper" />,
+  <App />,
   document.getElementById('main'),
 );
 
